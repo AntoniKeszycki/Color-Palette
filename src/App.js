aa8af1e3b0fa49b024627f10cc3ad2bc7e1c7ac2
Antoni Keszycki <a.keszycki@gmail.com>
2022-11-27 15:06:18 +0100

    Fix Palette Route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import Palette from "./Palette";
 import seedColor from "./seedColor";
 import { generatePalette } from "./colorHelpers";
+import { palette } from "@mui/system";
 
+const findPalette = (id) => seedColor.find((palette) => palette.id == id);
+const PaletteWrapper = () => {
+  const { id } = useParams();
+  const palette = generatePalette(findPalette(id));
+  return <Palette palette={palette} />;
+};
 class App extends Component {
   render() {
     return (
       <Routes>
         <Route path="/" element={<h1>Palette List</h1>} />
-        <Route
-          path="/palette/:id"
-          element={<Palette palette={generatePalette(seedColor[4])} />}
-        />
+        <Route path="/palette/:id" element={<PaletteWrapper />} />
       </Routes>
-      /*     <div className="App">
+      /*  
+      <div className="App">
         <Palette palette={generatePalette(seedColor[4])} />
-      </div> */
+      </div> 
+      */
     );
   }
 }
